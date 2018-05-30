2fabf1d8d40c0ddf261f9543bf4c8b0687bbd337
james ikua <ikuaj96@gmail.com>
2018-05-30 16:31:01 +0300

    initial commits

---
+++ src/main/java/App.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App{
+    public static void main(String[] args){
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        get("/", (request, respond) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/signup", (request, respond) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/signup.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/homepage", (request, respond) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/homepage.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+    }
+}
